4dec8bdc59d4cb23612c1dcab4f7a19adef430d8
natasha_wang <natasha.wang@shinda.com.tw>
2026-06-13 00:30:01 +0800

    #hotfix import vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://goodpickslab.com'),
@@ -33,9 +34,11 @@ export default function RootLayout({
   return (
     <html lang="zh-TW" className="h-full">
       <body className="min-h-full flex flex-col bg-white">
+        
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
+          <Analytics />
       </body>
     </html>
   )
